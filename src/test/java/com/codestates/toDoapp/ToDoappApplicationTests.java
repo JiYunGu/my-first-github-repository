dda3214638fd20dfbf843da78e4c61dbd2033ac3
root <root@DESKTOP-U0UB4BD.localdomain>
2023-02-14 20:30:32 +0900

    initial commit

---
+++ src/test/java/com/codestates/toDoapp/ToDoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.toDoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
